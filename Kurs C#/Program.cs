ad19a793ca0d0408f0aa4fa9e227ea062547706d
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-07-28 19:16:56 +0200

    Dodaj pliki projektów.

---
+++ Kurs C#/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Kurs_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert numbers: ");
+            int number;
+            int? max = null;
+            int sum = 0;
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                if(number > max)
+                {
+                    max = number;
+                }
+            } while (number != 0);
+            Console.WriteLine($"Sum of numbers: {sum}");
+            if (max is null)
+            {
+                Console.WriteLine("No vaalues entered");
+            }
+            else
+            {
+                Console.WriteLine($"Max value: {max}");
+            }
+        }
+    }
+}
