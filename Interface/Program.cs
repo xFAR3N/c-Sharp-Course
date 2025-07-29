1393d83d44bc0e65c4d0568201228ea02df69a35
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-07-29 20:00:08 +0200

    Interface project created

---
+++ Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        public interface IShape
+        {
+            public double CalculateArea();
+            public double CalculatePerimeter();
+
+        }
+        public class Square : IShape
+        {
+            private double a;
+            public Square(double a)
+            {
+                this.a = a;
+            }
+            public double CalculateArea()
+            {
+                return Math.Pow(a, 2);
+            }
+            public double CalculatePerimeter()
+            {
+                return 4 * a;
+            }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
