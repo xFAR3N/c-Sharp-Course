a8cd117a95b856d947855188ca520f2208aa375d
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-07-29 19:44:14 +0200

    Name adjustments

---
+++ Date Parse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Date_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Insert your birth year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert your birth month: ");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert your birth day: ");
+            int birthDay = int.Parse(Console.ReadLine());
+
+            DateTime Birthday = new DateTime(birthYear, birthMonth, birthDay);*/
+            Console.WriteLine("Podaj date urodzenia w formacie dd.MM.yyyy");
+            string dateofBirth = Console.ReadLine();
+
+            DateTime Birthday = DateTime.Parse(dateofBirth);
+
+            TimeSpan timeSpan = DateTime.Now - Birthday;
+            Console.WriteLine($"Days since your birthday: {Math.Round(timeSpan.TotalDays, 0)}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Inheritance
+{
+    public abstract class Vehicle
+    {
+        // TODO: zdefiniuj wspólne części klas pochodnych
+        public string Model { get; set; }
+        public abstract void Start();
+        public abstract void Stop();
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Start() => Console.WriteLine($"Starting the car: {Model}");
+        public override void Stop() => Console.WriteLine($"Stoppping the car: {Model}");
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public override void Start() => Console.WriteLine($"Starting the motorcycle: {Model}");
+        public override void Stop() => Console.WriteLine($"Stoppping the motorcycle: {Model}");
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Model = "BMW";
+            car.Start();
+            car.Stop();
+        }
+    }
+}
